10b725feacf27287f7ef287422c1910a046d63c8
Yibo G <nil.yibo@gmail.com>
2014-03-01 01:06:22 -0600

    Added load graph function.
    html5 load from file.
    File parser completed.

--- js/infection.js
+++ js/infection.js
@@ -11,7 +11,7 @@
     ********************************
 */
 
-// This is the event hanlder for 'Run' button
+// This is the onclick event hanlder for 'Run' button
 // Functionality: Start the spread of epidemics
 function run_click() {
     var iSelect = document.getElementById('infectionSelect');
@@ -40,7 +40,7 @@ function run_click() {
     tSelect.title = "";
 }
 
-// This is the event hanlder for 'Reload' button
+// This is the onclick event hanlder for 'Reload' button
 // Functionality: reload the page
 function reload_click() {
     location.reload();
@@ -80,7 +80,6 @@ function compareList(list1, list2) {
 */
 
 // Core function to simulate one round of epidemics spread
-
 function oneRoundSpread() {
     var nextRoundInfected = [];
     for (var i = 0; i < nodes.length; ++i)
@@ -152,7 +151,7 @@ function decrementLinkEndsMaxThreshold(link) {
 
 /*
     ********************************
-        Dropdown select box event handlers
+        Event handlers
     ********************************
 */
 
@@ -243,4 +242,156 @@ function thresholdSelect_changed() {
     var sel = document.getElementById('thresholdSelect');
     selected_node.threshold = sel.selectedIndex;
     restart();
+}
+
+// This is the onchange event handler for file selector
+// Functionality: Show the chosen file and update the graph accordingly
+function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
+    var output = [];  // files is a FileList of File objects. List some properties.
+    var f; // File
+    if (f = files[0]) {
+        output.push('<strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+            f.size, ' bytes, last modified: ',
+            f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a');
+    }
+    document.getElementById('list').innerHTML = output.join('');
+
+    var reader = new FileReader();
+    reader.onload = parseGraphFile;     // Parse the file once completed loading
+    reader.readAsText(f, 'UTF-8');      // Read the file as text
+}
+
+var fileContent;
+var firstLine;
+
+// This function will parse the uploaded file and update the graph accordingly
+function parseGraphFile(evt)
+{
+    // Back up current nodes/links list
+    var backup = [nodes, lastNodeId, links];
+    // Clear existing graph
+    nodes = [];
+    lastNodeId = 0;
+    links = [];
+    restart();
+
+    fileContent = evt.target.result;
+    // General validity check
+    if (fileContent.match(/[^0123456789\[\]\(\)\,\n\s]/g) != null)
+    {
+        parseGraphFailed(backup, 'Illegal character!\nOnly digits, brackets, parentheses and commas are allowed.');
+        return;
+    }
+    var countNewLines = fileContent.match(/\n/g);
+    if (countNewLines == null || countNewLines.length < 2)
+    {
+        parseGraphFailed(backup, 'Must have at least three lines.');
+        return;
+    }
+
+    // Get the first line
+    var newLine = fileContent.search('\n');
+    firstLine = fileContent.substr(0, newLine);
+    fileContent = fileContent.substr(newLine + 1);
+    // Get the second line
+    newLine = fileContent.search('\n');
+    var secondLine = fileContent.substr(0, newLine);
+    // Get the third line
+    fileContent = fileContent.substr(newLine + 1);
+    newLine = fileContent.search('\n');
+    var thirdLine = (newLine == -1) ? fileContent : fileContent.substr(0, newLine);
+    // Check the rest
+    if (newLine != -1)
+    {
+        fileContent = fileContent.substr(newLine + 1);
+        if (fileContent.length != 0)
+            alert('[Warning] Ignoring any input after line 3.');
+    }
+
+    // Check node list validity
+    var match1 = firstLine.match(/\,/g), match3 = thirdLine.match(/\,/g);
+    if ((match1 != null && match3 == null) || (match1 == null && match3 != null)    // one is null and the other is not
+        || (match1 != null && match3 != null && (match1.length != match3.length)) ) // both are not null and length differ
+    {
+        parseGraphFailed(backup, 'Number of nodes is not equal to number of thresholds.');
+        return;
+    }
+    var nodeCount = (firstLine.match(/\,/g) == null) ? 1 : (firstLine.match(/\,/g).length + 1);
+    var index1 = firstLine.search(/\[/g);
+    var index3 = thirdLine.search(/\[/g);
+    if (index1 == -1 || index3 == -1 || firstLine.search(/\]/g) == -1 || thirdLine.search(/\]/g) == -1)
+    {
+        parseGraphFailed(backup, 'Node/Threshold line is not a bracked-enclosed list.');
+        return;
+    }
+
+    // Start updating nodes list
+    for (var i = 0; i < nodeCount; ++i)
+    {
+        firstLine = firstLine.substr(index1 + 1);
+        thirdLine = thirdLine.substr(index3 + 1);
+        // The last one ends with ']'
+        index1 = firstLine.search((i == nodeCount - 1) ? ']' : ',');
+        index3 = thirdLine.search((i == nodeCount - 1) ? ']' : ',');
+        var currId = parseInt(firstLine.substr(0, index1));
+        var currThreshold = parseInt(thirdLine.substr(0, index3));
+        if (isNaN(currId) || isNaN(currThreshold))
+        {
+            parseGraphFailed(backup, 'Node id / Threshold is an illegal number.');
+            return;
+        }
+        nodes.push({id: currId, reflexive: false, infected: false, threshold: currThreshold});
+        ++lastNodeId;
+    }
+
+    // Check link list validity
+    if (secondLine.search(/\[/g) == -1 || secondLine.search(']') == -1)
+    {
+        parseGraphFailed(backup, 'Link line is not a bracked-enclosed list.');
+        return;
+    }
+
+    // Start updating links list
+    var linkCount = (secondLine.match(/\(/g) == null) ? 0 : (secondLine.match(/\(/g).length);
+    var index2 = secondLine.search(/\[/g);
+    for (var i = 0; i < linkCount; ++i)
+    {
+        index2 = secondLine.search(/\(/g);
+        secondLine = secondLine.substr(index2 + 1);
+        // The last one ends with ']'
+        index2 = secondLine.search(',');
+        var currSrc = parseInt(secondLine.substr(0, index2));
+        secondLine = secondLine.substr(index2 + 1);
+        index2 = secondLine.search(/\)/g);
+        var currTar = parseInt(secondLine.substr(0, index2));
+        secondLine = secondLine.substr(index2 + 1)
+        if (isNaN(currSrc) || isNaN(currTar))
+        {
+            parseGraphFailed(backup, 'Link Src/Dest is an illegal node id.');
+            return;
+        }
+        links.push({source: nodes[currSrc], target: nodes[currTar], left: true, right: true});
+    }
+
+    // init D3 force layout
+    force = d3.layout.force()
+        .nodes(nodes)
+        .links(links)
+        .size([width, height])
+        .linkDistance(150)
+        .charge(-500)
+        .on('tick', tick)
+
+    // Reload the graph
+    restart();
+}
+
+function parseGraphFailed(backup, message)
+{
+    nodes = backup[0];
+    lastNodeId = backup[1];
+    links = backup[2];
+    alert('[Error]: ' + message);
+    return;
 }
