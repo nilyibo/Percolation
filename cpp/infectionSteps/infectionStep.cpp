a1e7e37149b5ec94783403e3923c7080938d305b
Yibo G <nil.yibo@gmail.com>
2014-04-07 13:50:36 -0500

    Added output format.

--- cpp/infectionSteps/infectionStep.cpp
+++ cpp/infectionSteps/infectionStep.cpp
@@ -4,13 +4,12 @@
 * This function is used to calcualte N(n = 6, r, p) for percolation on regualr lattice
 */
 
+#include <stdio.h>
 #include <iostream>
-using namespace std;
 
 // Parameters
 #define	n	6	// side per face
 #define	r	3	// infection threhold
-// Initially infected probability
 #define	pmin	0.0
 #define	pmax	1.0
 #define	pstep	0.01
@@ -19,7 +18,20 @@ using namespace std;
 #define columns 10
 #define simulations 1000
 
+
+
 int main()
 {
+	printf("This function calculates N(%d, %d, p).\n\n", n, r);
+	printf("p, N(%d, %d, p).\n", n, r);
+	for (double p = pmin; p < pmax; p += pstep)
+	{
+		int steps = 0;
+		for (int i = 0; i < simulations; ++i)
+			steps += 0;
+		printf("%f, %f\n", p, (double)steps/simulations);
+	}
+
+	system("pause");
 	return 0;
 }
