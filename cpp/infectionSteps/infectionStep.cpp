f09374ab1533c1e5d52c6638ef23bf29daefdde8
Yibo G <nil.yibo@gmail.com>
2014-04-07 12:07:01 -0500

    Added c++ version prototype for N(n, r, p).

---
+++ cpp/infectionSteps/infectionStep.cpp
@@ -0,0 +1,25 @@
+/**
+* @file infectionStep.cpp
+* @author Yibo G (https://github.com/nilyibo)
+* This function is used to calcualte N(n = 6, r, p) for percolation on regualr lattice
+*/
+
+#include <iostream>
+using namespace std;
+
+// Parameters
+#define	n	6	// side per face
+#define	r	3	// infection threhold
+// Initially infected probability
+#define	pmin	0.0
+#define	pmax	1.0
+#define	pstep	0.01
+
+#define rows 10
+#define columns 10
+#define simulations 1000
+
+int main()
+{
+	return 0;
+}
