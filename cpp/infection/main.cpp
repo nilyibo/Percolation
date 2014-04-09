029ffe4f8a694e49f1579f19f5e2d79dfa290a0e
Yibo G <nilyibo@gmail.com>
2014-04-09 02:27:20 -0500

    Added linux program.

---
+++ cpp/infection/main.cpp
@@ -0,0 +1,168 @@
+/**
+* @file infectionStep.cpp
+* @author Yibo G (https://github.com/nilyibo)
+* This function is used to calcualte N(n = 6, r, p) for percolation on regualr lattice
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+#include <cmath>
+#include <omp.h>
+using namespace std;
+
+// Parameters
+#define	n	6	// side per face
+#define	r	3	// infection threhold
+#define	pmin	0.03
+#define	pmax	0.16
+#define	pstep	0.005
+
+#define rows 100
+#define columns 100
+#define simulations 1000
+
+int countInfectedNeighbor(bool grid[rows][columns], int x, int y)
+{
+	int count = 0;
+#if n == 4
+	if (x > 0)
+		count += (grid[x - 1][y]) ? 1 : 0;
+	if (y > 0)
+		count += (grid[x][y - 1]) ? 1 : 0;
+	if (x + 1 < rows)
+		count += (grid[x + 1][y]) ? 1 : 0;
+	if (y + 1 < columns)
+		count += (grid[x][y + 1]) ? 1 : 0;
+#elif n == 6
+	// Assume in hexagon grid,
+	// odd columns are half above even columns
+
+	// Same for both odd and even
+	if (x > 0)
+		count += (grid[x - 1][y]) ? 1 : 0;
+	if (x + 1 < rows)
+		count += (grid[x + 1][y]) ? 1 : 0;
+
+	if (y % 2 == 0) // Odd column
+	{
+		if (y > 0)
+		{
+			count += (grid[x][y - 1]) ? 1 : 0;
+			if (x > 0)
+				count += (grid[x - 1][y - 1]) ? 1 : 0;
+		}
+		if (y + 1 < columns)
+		{
+			count += (grid[x][y + 1]) ? 1 : 0;
+			if (x > 0)
+				count += (grid[x - 1][y + 1]) ? 1 : 0;
+		}
+	}
+	else	// Even column
+	{
+		if (y > 0)
+		{
+			count += (grid[x][y - 1]) ? 1 : 0;
+			if (x + 1 < rows)
+				count += (grid[x + 1][y - 1]) ? 1 : 0;
+		}
+		if (y + 1 < columns)
+		{
+			count += (grid[x][y + 1]) ? 1 : 0;
+			if (x + 1 < rows)
+				count += (grid[x + 1][y + 1]) ? 1 : 0;
+		}
+	}
+#endif
+	return count;
+}
+
+// Random initial seed
+void buildGrid(bool grid[rows][columns], double p)
+{
+	for (int i = 0; i < rows; ++i)
+		for (int j = 0; j < columns; ++j)
+			if ((double)rand() / RAND_MAX < p)
+				grid[i][j] = true;
+			else
+				grid[i][j] = false;
+}
+
+// Run one round of spread on the grid and set grid changed flag
+void oneRound(bool grid[rows][columns], bool & gridChanged)
+{
+	gridChanged = false;
+	bool newGrid[rows][columns];
+
+	for (int i = 0; i < rows; ++i)
+		for (int j = 0; j < columns; ++j)
+		{
+			newGrid[i][j] = grid[i][j];
+			if (!grid[i][j] && countInfectedNeighbor(grid, i, j) >= r)
+			{
+				gridChanged = true;
+				newGrid[i][j] = true;
+			}
+		}
+
+	for (int i = 0; i < rows; ++i)
+		for (int j = 0; j < columns; ++j)
+			grid[i][j] = newGrid[i][j];
+}
+
+
+// Run one simulation with the given probability
+// Returns number of steps
+int oneSimulation(double p)
+{
+	bool grid[rows][columns];
+	bool gridChanged = true;
+	int count = -1; // Counter is added once even if steps is 0
+
+	buildGrid(grid, p);
+	while (gridChanged)
+	{
+		oneRound(grid, gridChanged);
+		++count;
+	}
+
+	return count;
+}
+
+int main()
+{
+	// Initialize random seed
+	srand(static_cast<unsigned int>(time(NULL)));
+
+	printf("This function calculates N(%d, %d, p).\n\n", n, r);
+	printf("Parameters:\nrows = %d, columns = %d, #simulations = %d.\n",
+			rows, columns, simulations);
+	printf("p from %f to %f step %f.\n", pmin, pmax, pstep);
+	printf("\np, N(%d, %d, p), SD.\n", n, r);
+
+	clock_t t = clock();
+	int iMax = floor((pmax - pmin) / pstep);
+	for (int i = 0; i < iMax; ++i)
+	{
+		double p = pmin + i * pstep;
+		int steps = 0, steps2 = 0; // sum of x and sum of x^2
+//		#pragma omp parallel for
+		for (int i = 0; i < simulations; ++i)
+		{
+			int count = oneSimulation(p);
+			steps += count;
+			steps2 += count * count;
+		}
+		double avg = (double)steps / simulations;
+		double sd = sqrt((double)steps2 / simulations - avg * avg);
+		printf("%f, %f, %f", p, avg, sd);
+		cout << endl;
+	}
+
+	t = clock() - t;
+	printf("\nProcessor time: %f.\n", t / (double)CLOCKS_PER_SEC);
+
+	return 0;
+}
