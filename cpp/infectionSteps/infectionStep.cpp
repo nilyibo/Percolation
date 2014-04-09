8203528b0ad411d4744a814add8f25176c3af0a1
Yibo G <nil.yibo@gmail.com>
2014-04-09 15:34:26 -0500

    Changed to parallel version.

--- cpp/infectionSteps/infectionStep.cpp
+++ cpp/infectionSteps/infectionStep.cpp
@@ -23,18 +23,11 @@ using namespace std;
 #define	pstep	0.01
 #define simulations 100
 
-#define sizemin 5
-#define sizemax 200
-#define sizestep 5
+#define sizemin 10
+#define sizemax 100
+#define sizestep 10
 
-#define rows size
-#define columns size
-
-static int size;
-static bool ** grid;		// Current infection status
-static bool ** newGrid;	// temp: next round infection status
-
-int countInfectedNeighbor(int x, int y)
+int countInfectedNeighbor(bool ** grid, int x, int y, int size)
 {
 	int count = 0;
 #if n == 4
@@ -42,18 +35,18 @@ int countInfectedNeighbor(int x, int y)
 		count += (grid[x - 1][y]) ? 1 : 0;
 	if (y > 0)
 		count += (grid[x][y - 1]) ? 1 : 0;
-	if (x + 1 < rows)
+	if (x + 1 < size)
 		count += (grid[x + 1][y]) ? 1 : 0;
-	if (y + 1 < columns)
+	if (y + 1 < size)
 		count += (grid[x][y + 1]) ? 1 : 0;
 #elif n == 6
 	// Assume in hexagon grid,
-	// odd columns are half above even columns
+	// odd size are half above even size
 
 	// Same for both odd and even
 	if (x > 0)
 		count += (grid[x - 1][y]) ? 1 : 0;
-	if (x + 1 < rows)
+	if (x + 1 < size)
 		count += (grid[x + 1][y]) ? 1 : 0;
 
 	if (y % 2 == 0) // Odd column
@@ -64,7 +57,7 @@ int countInfectedNeighbor(int x, int y)
 			if (x > 0)
 				count += (grid[x - 1][y - 1]) ? 1 : 0;
 		}
-		if (y + 1 < columns)
+		if (y + 1 < size)
 		{
 			count += (grid[x][y + 1]) ? 1 : 0;
 			if (x > 0)
@@ -76,13 +69,13 @@ int countInfectedNeighbor(int x, int y)
 		if (y > 0)
 		{
 			count += (grid[x][y - 1]) ? 1 : 0;
-			if (x + 1 < rows)
+			if (x + 1 < size)
 				count += (grid[x + 1][y - 1]) ? 1 : 0;
 		}
-		if (y + 1 < columns)
+		if (y + 1 < size)
 		{
 			count += (grid[x][y + 1]) ? 1 : 0;
-			if (x + 1 < rows)
+			if (x + 1 < size)
 				count += (grid[x + 1][y + 1]) ? 1 : 0;
 		}
 	}
@@ -91,25 +84,29 @@ int countInfectedNeighbor(int x, int y)
 }
 
 // Random initial seed
-void buildGrid(double p)
+void buildGrid(bool ** grid, double p, int size)
 {
-	for (int i = 0; i < rows; ++i)
-		for (int j = 0; j < columns; ++j)
+	for (int i = 0; i < size; ++i)
+		for (int j = 0; j < size; ++j)
+		{
 			if ((double)rand() / RAND_MAX < p)
+			{
 				grid[i][j] = true;
+			}
 			else
 				grid[i][j] = false;
+		}
 }
 
 // Run one round of spread on the grid and set grid changed flag
-void oneRound(bool & gridChanged)
+void oneRound(bool ** grid, bool ** newGrid, bool & gridChanged, int size)
 {
 	gridChanged = false;
 
-	for (int i = 0; i < rows; ++i)
-		for (int j = 0; j < columns; ++j)
+	for (int i = 0; i < size; ++i)
+		for (int j = 0; j < size; ++j)
 		{
-			if (!grid[i][j] && countInfectedNeighbor(i, j) >= r)
+			if (!grid[i][j] && countInfectedNeighbor(grid, i, j, size) >= r)
 			{
 				gridChanged = true;
 				newGrid[i][j] = true;
@@ -118,23 +115,23 @@ void oneRound(bool & gridChanged)
 				newGrid[i][j] = grid[i][j];
 		}
 
-	for (int i = 0; i < rows; ++i)
-		for (int j = 0; j < columns; ++j)
+	for (int i = 0; i < size; ++i)
+		for (int j = 0; j < size; ++j)
 			grid[i][j] = newGrid[i][j];
 }
 
 
 // Run one simulation with the given probability
 // Returns number of steps
-int oneSimulation(double p)
+int oneSimulation(bool ** grid, bool ** newGrid, double p, int size)
 {
 	bool gridChanged = true;
 	int count = -1; // Counter is added once even if steps is 0
 
-	buildGrid(p);
+	buildGrid(grid, p, size);
 	while (gridChanged)
 	{
-		oneRound(gridChanged);
+		oneRound(grid, newGrid, gridChanged, size);
 		++count;
 	}
 
@@ -162,47 +159,53 @@ int main()
 
 	output << "p, N(p), SD, size.\n" << endl;
 
-	for (size = sizemin; size <= sizemax; size += sizestep)
+#pragma omp parallel for num_threads(8)
+	for (int size = sizemin; size <= sizemax; size += sizestep)
 	{
 		cout << "size = " << size << endl;
 		clock_t t = clock();
 
-		grid = new bool*[rows];
-		for (int i = 0; i < rows; ++i)
-			grid[i] = new bool[columns];
-		newGrid = new bool*[rows];
-		for (int i = 0; i < rows; ++i)
-			newGrid[i] = new bool[columns];
+		bool ** grid = new bool*[size];
+		for (int i = 0; i < size; ++i)
+			grid[i] = new bool[size];
+		bool ** newGrid = new bool*[size];
+		for (int i = 0; i < size; ++i)
+			newGrid[i] = new bool[size];
 
 		for (double p = pmin; p <= pmax; p += pstep)
 		{
 			int steps = 0, steps2 = 0; // sum of x and sum of x^2
 			for (int i = 0; i < simulations; ++i)
 			{
-				int count = oneSimulation(p);
+				int count = oneSimulation(grid, newGrid, p, size);
 				steps += count;
 				steps2 += count * count;
 			}
 			double avg = (double)steps / simulations;
 			double sd = sqrt((double)steps2 / simulations - avg * avg);
-			output << p << ", " << avg << ", " << sd << ", " << size << endl;
+#pragma omp critical
+			{
+				output << p << ", " << avg << ", " << sd << ", " << size << endl;
+			}
 		}
 
-		for (int i = 0; i < rows; ++i)
+		for (int i = 0; i < size; ++i)
 			delete[] grid[i];
 		delete[] grid;
 		grid = NULL;
-		for (int i = 0; i < rows; ++i)
+		for (int i = 0; i < size; ++i)
 			delete[] newGrid[i];
 		delete[] newGrid;
 		newGrid = NULL;
 
 		t = clock() - t;
-
-		output << "\nProcessor time (" << size << "): "
-			<< t / (double)CLOCKS_PER_SEC << ".\n" << endl;
-		cout << "\nProcessor time (" << size << "): "
-			<< t / (double)CLOCKS_PER_SEC << ".\n" << endl;
+#pragma omp critical
+		{
+			output << "\nProcessor time (" << size << "): "
+				<< t / (double)CLOCKS_PER_SEC << ".\n" << endl;
+			cout << "\nProcessor time (" << size << "): "
+				<< t / (double)CLOCKS_PER_SEC << ".\n" << endl;
+		}
 	}
 
 	system("pause");
